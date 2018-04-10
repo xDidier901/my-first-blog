f583fc9c6faf6ac2943dbd47c70d780a427fab5f
xDidier901 <xdidier901@gmail.com>
2018-04-10 12:36:37 -0700

    Added HTML to the site

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+     url(r'', include('blog.urls')),
 ]
